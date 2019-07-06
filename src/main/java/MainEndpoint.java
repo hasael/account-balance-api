899feadb87dcde649a468378fc44020bd0bb83fc
Hasa Elman <elman.hasa@ynap.com>
2019-07-06 14:15:23 +0200

    Remove unused models and services

--- src/main/java/MainEndpoint.java
+++ src/main/java/MainEndpoint.java
@@ -1,30 +1,14 @@
-import com.google.gson.Gson;
-import domain.abstractions.UserService;
-import response.StandardResponse;
-import response.StatusResponse;
-import services.UserServiceImpl;
-
 import static spark.Spark.get;
 
 public class MainEndpoint {
-    private static UserService userService;
 
     public static void main(String[] args) {
         buildServices();
 
-        get("/hello", (req, res) -> "Hello, world");
-
-        get("/hello/:name", (req, res) -> "Hello, " + req.params(":name"));
-        get("/hello/:name", (req, res) -> "Hello, " + req.params(":name"));
-        get("/users", (request, response) -> {
-            response.type("application/json");
-
-            return new Gson().toJson(
-                    new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUsers())));
-        });
+        get("/TestConnection", (req, res) -> "Up and running");
     }
 
     private static void buildServices(){
-        userService = new UserServiceImpl();
+
     }
 }
